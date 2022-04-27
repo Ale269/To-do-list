fba108595e383915f43751e07ae619d898cd9c01
Alessio <alessio26manfredini@gmail.com>
2022-04-27 21:43:35 +0200

    Create dropdown menu style

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,12 @@
 import "../style/style.scss";
 
+document.querySelector("button").addEventListener("click", () => {
+    console.log("start");
+
+    if(document.querySelector(".project-container").classList.contains("active")){
+        document.querySelector(".project-container").setAttribute("class", "project-container");
+        return;
+    }
+
+    document.querySelector(".project-container").classList.add("active");
+});
