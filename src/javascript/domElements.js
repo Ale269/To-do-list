687393769069f781a7d6ee03b1f596a87aaf6255
Alessio <alessio26manfredini@gmail.com>
2022-04-29 17:08:53 +0200

    Set nav input display logic

--- src/javascript/domElements.js
+++ src/javascript/domElements.js
@@ -1,5 +1,9 @@
 const domElements = {
     addProjectBtn: document.querySelector(".add-project-button"),
+    projectInputField: document.querySelector(".input-container"),
+    addProjectDefault: document.querySelector(".add-project-default-btn"),
+    cancelProjectBtn: document.getElementById("cancel-btn-project"),
+    confirmProjectBtn: document.getElementById("confirm-btn-project"),
 }
 
 
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -21,10 +21,24 @@ class Project {
 
 
 const Controller = (() => {
+    const displayProjectCreator = function() {
+        domElements.addProjectDefault.style.cssText = "display: none;";
+        domElements.projectInputField.style.cssText = "display: block;";
+    }
+
+    const removeProjectCreator = function(e) {
+        e.stopPropagation() 
+        domElements.addProjectDefault.setAttribute("style", "");
+        domElements.projectInputField.setAttribute("style", "");
+    }
+
+
     domElements.addProjectBtn.addEventListener("click", displayProjectCreator);
+    domElements.cancelProjectBtn.addEventListener("click", (event) => {
+        removeProjectCreator(event);
+    });
 
 
-    
 })();
 
 
